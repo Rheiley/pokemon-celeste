74e1c73892cb1b9ab7f88e459a2c1742abf83fc0
Rheiley <118871025+Rheiley@users.noreply.github.com>
2024-09-03 09:27:38 -0700

    Update Arnica Town merchants

--- Data/Scripts/023_NPCs/Merchants/ArnicaTownMerchants.rb
+++ Data/Scripts/023_NPCs/Merchants/ArnicaTownMerchants.rb
@@ -2,6 +2,7 @@ def arnicaTownMerchant1
   setPrice(:SWEETAPPLE, 1000)
   setPrice(:TARTAPPLE, 1000)
   setPrice(:SYRUPYAPPLE, 1500)
+  
   pbPokemonMart([
     :SWEETAPPLE,
     :TARTAPPLE,
@@ -10,3 +11,37 @@ def arnicaTownMerchant1
   speech = "Welcome! I sell fresh produce!", 
   cantsell = true)
 end
+
+def arnicaTownMerchant2
+  setPrice(:ORANBERRY, 300)
+  setPrice(:PECHABERRY, 300)
+  setPrice(:RAWSTBERRY, 300)
+  setPrice(:CHERIBERRY, 300)
+  setPrice(:CHESTOBERRY, 300)
+
+  pbPokemonMart([
+    :ORANBERRY,
+    :PECHABERRY,
+    :RAWSTBERRY,
+    :CHERIBERRY,
+    :CHESTOBERRY
+  ], 
+  speech = "Get your berries here!", 
+  cantsell = true)
+end
+
+def arnicaTownMerchant3
+  setPrice(:FRESHWATER, 500)
+  setPrice(:SODAPOP, 600)
+  setPrice(:LEMONADE, 750)
+
+  pbPokemonMart([
+    :FRESHWATER,
+    :SODAPOP,
+    :LEMONADE
+  ], 
+  speech = "Cold drinks for sale here!", 
+  cantsell = true)
+end
+
+
